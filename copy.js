0eb24bd12087948a26396d102c5820687b11f401
shadowwreck <141768289+shadowwreck@users.noreply.github.com>
2023-08-08 19:10:01 -0500

    Update copy.js
    Added a failback check to see if running. 
    Also took the amount of server max ram, and divided by the amount the script uses.   This inturn will initiate the script on the hostname with max threads.

--- copy.js
+++ copy.js
@@ -1,13 +1,42 @@
 /** @param {NS} ns */
 export async function copy(ns, hostname) {
   const filesToCopy = ["cryptnet/root.js", "cryptnet/auto-hack.js", "cryptnet/start.js", "cryptnet/copy.js"];
+  const mainscript = "cryptnet/start.js";
 
-  for (const files of filesToCopy) {
-    const fileExists = await ns.fileExists(files);
+  let scriptExists = false;
+
+  for (const file of filesToCopy) {
+    const fileExists = await ns.fileExists(file);
     if (fileExists) {
-      await ns.scp(files, hostname);
+      scriptExists = true;
+      break; // Exit the loop if at least one script exists
     }
   }
 
-  await ns.exec("cryptnet/start.js", hostname);
+  if (scriptExists) {
+    ns.print("!Script exists on target. Executing");
+    if (!ns.scriptRunning(mainscript, hostname)) {
+      ns.print("Script is not running on target. Executing");
+      await ns.exec(mainscript, hostname);
+    }
+  } else {
+    ns.print(`Scripts do not exist on target ${hostname}! Copying Scripts`);
+
+    for (const file of filesToCopy) {
+      await ns.scp(file, hostname, "home"); // Copy files from home to hostname
+    }
+    if (!ns.scriptRunning(mainscript, hostname)) {
+      ns.print("Script is not running on target. Executing");
+      await ns.exec(mainscript, hostname);
+    }
+  }
+
+  const ramNeeded = ns.getScriptRam(mainscript);
+  const availableRam = ns.getServerMaxRam(hostname);
+  const maxThreads = Math.floor(availableRam / ramNeeded);
+
+  if (maxThreads >= 1) {
+    ns.print(`Running script with max threads (${maxThreads}) on ${hostname}`);
+    await ns.exec(mainscript, hostname, maxThreads);
+  }
 }
