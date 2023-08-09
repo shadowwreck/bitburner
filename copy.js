61c1cacd140ffea9a731ebd9a4615b0783193f60
shadowwreck <141768289+shadowwreck@users.noreply.github.com>
2023-08-09 08:37:19 -0500

    Shortened Script

--- copy.js
+++ copy.js
@@ -1,42 +1,37 @@
 /** @param {NS} ns */
 export async function copy(ns, hostname) {
-  const filesToCopy = ["cryptnet/root.js", "cryptnet/auto-hack.js", "cryptnet/start.js", "cryptnet/copy.js"];
+  const filesToCopy = ["cryptnet/root.js", "cryptnet/auto-hack.js", "cryptnet/start.js", "cryptnet/copy.js", "hostnames.txt"];
   const mainscript = "cryptnet/start.js";
 
   let scriptExists = false;
-
+  // Check if all four files exist
+  let existingFilesCount = 0;
   for (const file of filesToCopy) {
     const fileExists = await ns.fileExists(file);
     if (fileExists) {
-      scriptExists = true;
-      break; // Exit the loop if at least one script exists
+      existingFilesCount++;
+      if (existingFilesCount === 4) {
+        scriptExists = true;
+        break;
+      }
     }
   }
 
   if (scriptExists) {
-    ns.print("!Script exists on target. Executing");
     if (!ns.scriptRunning(mainscript, hostname)) {
-      ns.print("Script is not running on target. Executing");
-      await ns.exec(mainscript, hostname);
+      const ramNeeded = ns.getScriptRam(mainscript);
+      const availableRam = ns.getServerMaxRam(hostname);
+      const maxThreads = Math.floor(availableRam / ramNeeded);
+
+      if (maxThreads >= 1) {
+        ns.print(`Script is not running on ${hostname}. Executing with max threads (${maxThreads})`);
+        ns.scp(filesToCopy, hostname);
+        ns.exec(mainscript, hostname, maxThreads);
+      }
     }
   } else {
-    ns.print(`Scripts do not exist on target ${hostname}! Copying Scripts`);
-
-    for (const file of filesToCopy) {
-      await ns.scp(file, hostname, "home"); // Copy files from home to hostname
-    }
-    if (!ns.scriptRunning(mainscript, hostname)) {
-      ns.print("Script is not running on target. Executing");
-      await ns.exec(mainscript, hostname);
-    }
-  }
-
-  const ramNeeded = ns.getScriptRam(mainscript);
-  const availableRam = ns.getServerMaxRam(hostname);
-  const maxThreads = Math.floor(availableRam / ramNeeded);
-
-  if (maxThreads >= 1) {
-    ns.print(`Running script with max threads (${maxThreads}) on ${hostname}`);
-    await ns.exec(mainscript, hostname, maxThreads);
+    ns.print("Let us ensure the scripts are copied");
+    ns.scp(filesToCopy, hostname);
+    ns.print("Scripts copied.");
   }
 }
