727289d17e4d4dd8896b8db21d5b5d74be5abce1
shadowwreck <141768289+shadowwreck@users.noreply.github.com>
2023-08-09 08:37:58 -0500

    Fixed issues
    Resolved Functions not applying as they should

--- start.js
+++ start.js
@@ -15,36 +15,16 @@ export async function main(ns) {
     const isStartScriptRunning = ns.scriptRunning(mainscript, hostname);
     const isStartScriptFileExists = ns.fileExists(mainscript, hostname);
 
-    // If we do not have root on the target. Run rootscript to gain root
     if (!ns.hasRootAccess(hostname)) {
       await rootscript(ns, hostname);
-    }
-
-    //If we have root on the target, and the required hack level is the same or less then ours. Hack them
-    if (ns.hasRootAccess(hostname)) {
-      if (myhacklvl >= targetHackingLevel) {
-        await hackscript(ns, hostname);
-      }
-    }
-    //If the script is running and/or exists on the target do the following
-    if (!isStartScriptRunning && !isStartScriptFileExists) {
-      const availableram = ns.getServerMaxRam(hostname);
-      if (availableram >= ramneeded) {
-        await copy(ns, hostname);
-      } else {
-        ns.print(`Not enough available RAM on: ${hostname}! to copy and execute`);
-      }
     } else {
-      ns.print('The Script is either running or already exists on host. Moving on:');
-      ns.print('Lets ensure its not running'); 
-      // All this does, is double check to make sure nothing was missed when we ran our check
-     if (!isStartScriptRunning) {
       const availableram = ns.getServerMaxRam(hostname);
       if (availableram >= ramneeded) {
         await copy(ns, hostname);
+        if (myhacklvl >= targetHackingLevel) {
+          await hackscript(ns, hostname);
+        }
       }
-      }
-    
     }
   }
 }
