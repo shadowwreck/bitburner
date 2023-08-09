3bd84ecc24a286623e1b894dbf14d3da7ae1ef49
shadowwreck <141768289+shadowwreck@users.noreply.github.com>
2023-08-08 19:10:26 -0500

    Update start.js
    Addressed issues where code was not being activated upon their applied condition.

--- start.js
+++ start.js
@@ -3,39 +3,48 @@ import { rootscript } from "cryptnet/root.js";
 import { copy } from "cryptnet/copy.js"; // Modified import for default export
 
 export async function main(ns) {
-
-
-
+  const mainscript = "cryptnet/start.js";
   const filename = "hostnames.txt";
   const content = ns.read(filename);
   const hostnames = content.split('\n').map(hostname => hostname.trim()).filter(hostname => hostname !== '');
-  const ramneeded = ns.getScriptRam("cryptnet/start.js");
+  const ramneeded = ns.getScriptRam(mainscript);
 
   for (const hostname of hostnames) {
     const myhacklvl = ns.getHackingLevel();
     const targetHackingLevel = ns.getServerRequiredHackingLevel(hostname);
-    const isStartScriptRunning = ns.scriptRunning("start.js", hostname);
-    const isStartScriptFileExists = ns.fileExists("start.js", hostname);
-    
-    if (!ns.hasRootAccess(hostname)|| ns.getServerNumPortsRequired(hostname) > 0) {
+    const isStartScriptRunning = ns.scriptRunning(mainscript, hostname);
+    const isStartScriptFileExists = ns.fileExists(mainscript, hostname);
+
+    // If we do not have root on the target. Run rootscript to gain root
+    if (!ns.hasRootAccess(hostname)) {
       await rootscript(ns, hostname);
     }
-    
+
+    //If we have root on the target, and the required hack level is the same or less then ours. Hack them
     if (ns.hasRootAccess(hostname)) {
       if (myhacklvl >= targetHackingLevel) {
         await hackscript(ns, hostname);
       }
     }
-    
+    //If the script is running and/or exists on the target do the following
     if (!isStartScriptRunning && !isStartScriptFileExists) {
       const availableram = ns.getServerMaxRam(hostname);
       if (availableram >= ramneeded) {
         await copy(ns, hostname);
       } else {
-        ns.print('Not enough available RAM on ${hostname} to copy and execute.');
+        ns.print(`Not enough available RAM on: ${hostname}! to copy and execute`);
       }
     } else {
       ns.print('The Script is either running or already exists on host. Moving on:');
+      ns.print('Lets ensure its not running'); 
+      // All this does, is double check to make sure nothing was missed when we ran our check
+     if (!isStartScriptRunning) {
+      const availableram = ns.getServerMaxRam(hostname);
+      if (availableram >= ramneeded) {
+        await copy(ns, hostname);
+      }
+      }
+    
     }
   }
 }
