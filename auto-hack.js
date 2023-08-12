3f818b7817ff52d9d14416adb527995df22041c0
shadowwreck <141768289+shadowwreck@users.noreply.github.com>
2023-08-12 09:58:36 -0500

    Major cleanup
    Fully revised code to simplify, and reduce ram

--- auto-hack.js
+++ auto-hack.js
@@ -1,31 +1,5 @@
 export async function hackscript(ns, hostname) {
-const moneybeforehackfile = "moneybefore.txt"
-const moneyafterhackfile = "moneyafter.txt"
-
-  const purchased = ns.getPurchasedServers();
-  ns.write("purchasedservers.txt", purchased.join('\n'));
-  const blacklist = ["home", ...purchased]; // Add "home" and purchased servers to blacklist
-
-  // Check if the hostname is in the blacklist
-  if (blacklist.includes(hostname)) {
-    ns.print(`Skipping blacklisted server: ${hostname}`);
-    return;
+    console.log("hackscript - hacking:", hostname);
+    await ns.hack(hostname);
+    await ns.sleep(1000); // Optional sleep if needed
   }
-
-  const moneyBeforeHack = ns.getServerMoneyAvailable(hostname);
-  ns.write(moneybeforehackfile, moneyBeforeHack.toString());
-  await ns.hack(hostname);
-  const moneyAfterHack = ns.getServerMoneyAvailable(hostname);
-  ns.write(moneyafterhackfile, moneyAfterHack.toString());
-
-  const before = parseFloat(ns.read(moneybeforehackfile));
-  const after = parseFloat(ns.read(moneyafterhackfile));
-  const hackSuccess = after < before;
-  if (hackSuccess) {
-    ns.print(`!We have gained money from: ${hostname}! Growing target`);
-    await ns.grow(hostname);
-  } else {
-    ns.print('!We did not hack, we gained no money. Weaken target!');
-    await ns.weaken(hostname);
-  }
-}
